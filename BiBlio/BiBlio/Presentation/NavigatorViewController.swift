95b2005cde7767b42c3b1141cc3f21abfa892e5b
HendEl-Mahdy <hendm8042@gmail.com>
2025-04-26 12:12:11 -0700

    Initial project setup: configure app icon, finalize launch screen, and create base architecture

---
+++ BiBlio/BiBlio/Presentation/NavigatorViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NavigatorViewController.swift
+//  BiBlio
+//
+//  Created by Hend El Mahdy on 26/04/2025.
+//
+
+import UIKit
+
+class NavigatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
